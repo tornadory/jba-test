73fdae9ecb913a608fce1b54a61c710167e14a41
alizhang <tornadory@gmail.com>
2015-04-20 22:31:52 +0800

    Refactor **JSP files add user-account.jsp, remove "New Blog" in users detailed information **Controllers add RegsiterController&&AdminController

--- src/main/java/com/test/jba/controller/UserController.java
+++ src/main/java/com/test/jba/controller/UserController.java
@@ -29,55 +29,17 @@ public class UserController {
 	@Autowired
 	private BlogService blogService;
 	
-	@ModelAttribute("user")
-	public User constructUser(){
-		return new User();
-	}
-	
 	@ModelAttribute("blog")
 	public Blog constructBlog(){
 		return new Blog();
 	}
 	
-	@RequestMapping("/users")
-	public String users(Model model){
-		//System.out.print("userService.findAll num: " + userService.findAll().size());
-		model.addAttribute("users", userService.findAll()); 
-		//起作用，但仅在classic.jsp中  。。 在users.jsp里面没有这个变量
-		return "users";
-		//return "/WEB-INF/jsp/users.jsp";
-		//return "/WEB-INF/jsp/classic.jsp"; //OK
-	}
-	
-	@RequestMapping("/users/{id}")
-	public String detail(Model model, @PathVariable int id){
-		System.out.print("id: "+id);
-		//model.addAttribute("user", userService.findOne(id));
-		model.addAttribute("user", userService.findOneWithBlogs(id));
-		return "user-detail";
-		//return "/WEB-INF/jsp/user-detail-new.jsp"; //OK
-		//return "/WEB-INF/jsp/user-detail.jsp";
-	}
-	
-	@RequestMapping("/register")
-	public String showRegister(){
-		return "user-register";
-	}
-	
-	@RequestMapping(value="/register", method=RequestMethod.POST)
-	public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result){
-		if(result.hasErrors()){
-			return "user-register";
-		}
-		userService.save(user);
-		return "redirect:/register.html?success=true";
-	}
- 
+
 	@RequestMapping("/account")
 	public String account(Model model, Principal principal){
 		String name = principal.getName();
 		model.addAttribute("user", userService.findOneWithBlogs(name));
-		return "user-detail";
+		return "account";
 	}
 	
 	@RequestMapping(value="/account", method=RequestMethod.POST)
@@ -95,11 +57,5 @@ public class UserController {
 		Blog blog = blogService.findOne(id);
 		blogService.delete(blog);
 		return "redirect:/account.html";
-	}
-	
-	@RequestMapping("/users/remove/{id}")
-	public String removeUser(@PathVariable int id){
-		userService.delete(id);
-		return "redirect:/users.html";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/user-account.jsp
+++ src/main/webapp/WEB-INF/jsp/user-account.jsp
@@ -18,8 +18,49 @@ Web容器默认isELIgnored＝"false"。
 -->
 
 
-<h1>User: <c:out value="${user.name}" /></h1>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal"
+	data-target="#myModal">New Blog</button>
 
+<form:form commandName="blog" cssClass="form-horizontal">
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog"
+		aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"
+						aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h4 class="modal-title" id="myModalLabel">New Blog</h4>
+				</div>
+				<div class="modal-body">
+
+					<div class="form-group">
+						<label for="name" class="col-sm-2 control-label">Name:</label>
+						<div class="col-sm-10">
+							<form:input path="name" cssClass="form-control" />
+							<form:errors path="name" />
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="url" class="col-sm-2 control-label">URL:</label>
+						<div class="col-sm-10">
+							<form:input path="url" cssClass="form-control" />
+							<form:errors path="url" />
+						</div>
+					</div>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<input type="submit" class="btn btn-primary" value="Save" />
+				</div>
+			</div>
+		</div>
+	</div>
+</form:form>
 
 <br />
 <br />
